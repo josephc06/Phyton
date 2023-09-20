0cd5253e923e745f0be614665b1dec8203fa941a
josephc06 <144927989+josephc06@users.noreply.github.com>
2023-09-20 10:09:29 -0500

    Add files via upload

---
+++ Ejercicio 8 .py
@@ -0,0 +1,28 @@
+#los ciclos propuesto para programacion son secuenciales que permiten la realizacion o ejecutar +
+#funciones para poder abreviar o automatizar cofigo, esta el ciclo for que significa 
+#ciclo while que significa mientras que 
+print("Tipo de programa que incluya un ciclo")
+
+print("----------Inicio de ciclo--------")
+
+for i in range(10):
+    print(i*2)
+
+print("--------Finalice el ciclo-----")
+
+#El programa que vamos a realizar tiene como finalidad realizar un ciclo en donde le pida al usuario 
+#ingresar hasta 3 un numero en este caso la edad, y cuando genere la variable correcta ejecute en pantalla la sisguiente accion,
+#por ejemplo digita tu documento, 3. Digite si desea saber el numero de silla donde fue asignado 
+
+
+print("Admision para entrar")
+
+Nombre = str(input("Ingrese su nombre"))
+Edad = int(input("Ingresa tu edad"))
+identificacion = int(input("Ingrese su numero de documento"))
+
+#Asignacion ed ubicacion
+
+Puesto = 0 
+
+list = ["Fila 1O" "Fila 2M" "Fila 3H"] 
